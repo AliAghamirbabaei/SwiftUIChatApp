5601f32c0ef4538a37a51ba08c50e4d7e9873464
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2021-12-12 17:53:51 +0330

    Initial Commit

---
+++ SwiftUIChatTutorial/SwiftUIChatTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatTutorialApp.swift
+//  SwiftUIChatTutorial
+//
+//  Created by Ali Aghamirbabaei on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
