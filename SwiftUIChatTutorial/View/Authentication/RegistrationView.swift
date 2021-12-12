4e4f42a95e3f0740c86c671ef621e6ac50907933
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2021-12-12 18:13:58 +0330

    Create RegistrationView

---
+++ SwiftUIChatTutorial/View/Authentication/RegistrationView.swift
@@ -0,0 +1,20 @@
+//
+//  RegistrationView.swift
+//  SwiftUIChatTutorial
+//
+//  Created by Ali Aghamirbabaei on 12/12/21.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text("asd")
+    }
+}
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
+}
