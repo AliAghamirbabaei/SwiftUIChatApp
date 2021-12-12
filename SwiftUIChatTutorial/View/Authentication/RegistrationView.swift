87e71cbeac0ce212cabf148c8a86c01168a4fa44
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2021-12-12 19:24:33 +0330

    Initial RegistrationView

--- SwiftUIChatTutorial/View/Authentication/RegistrationView.swift
+++ SwiftUIChatTutorial/View/Authentication/RegistrationView.swift
@@ -8,8 +8,81 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    @State private var email = ""
+    @State private var username = ""
+    @State private var fullname = ""
+    @State private var password = ""
+    @Environment(\.presentationMode) var mode
+    
     var body: some View {
-        Text("asd")
+        VStack {
+            VStack(alignment: .leading, spacing: 12) {
+                HStack { Spacer() }
+                
+                Text("Get started.")
+                    .font(.largeTitle)
+                    .bold()
+                
+                Text("Creat your account")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(.blue)
+                
+                VStack(spacing: 20) {
+                    CustomTextField(imageName: "envelope",
+                                    placeholderText: "Email",
+                                    isSecureField: false,
+                                    text: $email)
+                    
+                    CustomTextField(imageName: "person",
+                                    placeholderText: "Username",
+                                    isSecureField: false,
+                                    text: $username)
+                    
+                    CustomTextField(imageName: "envelope",
+                                    placeholderText: "Fullname",
+                                    isSecureField: false,
+                                    text: $fullname)
+                    
+                    CustomTextField(imageName: "lock",
+                                    placeholderText: "Password",
+                                    isSecureField: true,
+                                    text: $password)
+                }
+                .padding([.top, .horizontal], 32)
+                
+            }
+            .padding(.leading)
+            
+            Button(action: {
+                print("asd")
+            }, label: {
+                Text("Sign Up")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 340, height: 50)
+                    .background(Color.blue)
+                    .clipShape(Capsule())
+                    .padding()
+            })
+                .padding(.top, 24)
+                .shadow(color: .gray, radius: 10, x: 0.0, y: 0.0)
+            
+            Spacer()
+            
+            Button(action: {
+                mode.wrappedValue.dismiss()
+            }, label: {
+                HStack {
+                    Text("Already have an account?")
+                        .font(.system(size: 14))
+                    
+                    Text("Sign In")
+                        .font(.system(size: 14, weight: .semibold))
+                }
+            })
+                .padding(.bottom, 32)
+        }
     }
 }
 
