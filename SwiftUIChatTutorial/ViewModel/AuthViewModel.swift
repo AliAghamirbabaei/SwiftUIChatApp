31632106124aa60f6edd1c5b9daf1a253330fd19
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2021-12-16 18:15:17 +0330

    Register user

--- SwiftUIChatTutorial/ViewModel/AuthViewModel.swift
+++ SwiftUIChatTutorial/ViewModel/AuthViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by Ali Aghamirbabaei on 12/16/21.
 //
 
-import Foundation
+import Firebase
 
 class AuthViewModel: NSObject, ObservableObject {
     
@@ -13,8 +13,15 @@ class AuthViewModel: NSObject, ObservableObject {
         
     }
     
-    func register() {
-        
+    func register(withEmail email: String, password: String, fullname: String, username: String) {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("DEBUG: Failed to register with error: \(error.localizedDescription)")
+                return
+            }
+            
+            print("DEBUG: Successfully registered user with Firebase!")
+        }
     }
     
     func uploadProfileImage() {
