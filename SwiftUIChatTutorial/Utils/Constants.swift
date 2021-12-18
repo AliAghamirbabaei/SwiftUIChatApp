f8eeff22b71e28d0d8631e936ec5b0024823a4b8
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2021-12-18 13:52:27 +0330

    Create constant file

---
+++ SwiftUIChatTutorial/Utils/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  SwiftUIChatTutorial
+//
+//  Created by Ali Aghamirbabaei on 12/18/21.
+//
+
+import Firebase
+
+let COLLECTION_USERS = Firestore.firestore().collection("users")
